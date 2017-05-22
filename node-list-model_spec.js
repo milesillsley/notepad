1222dcf846305ab04aae111ac0a3ab33d5a7391b
BenNoonan1991 <ben.noonan2@gmail.com>
2017-05-22 16:43:47 +0100

    Adds test for note list

---
+++ node-list-model_spec.js
@@ -0,0 +1,13 @@
+(function(exports) {
+  function testListOfNotes() {
+    var list = new List();
+    this.list = [];
+
+    if (this.list) === [] {
+      throw new Error("Please enter a note");
+    } else {
+      return this.list;
+    }
+  }
+  testListOfNotes();
+})(this);
