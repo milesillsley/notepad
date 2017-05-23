142b7b0eafa0c3659ba211e438aa611bb59074cd
milesillsley <milesillsley@hotmail.co.uk>
2017-05-23 19:01:46 +0100

    note list view displays an html list of all the notes in a list of notes

--- note-list-view.js
+++ note-list-view.js
@@ -1,10 +1,13 @@
 (function(exports) {
-   function noteListView(list) {
-     this.list = list;
-  }
-     this.prototype.displayPage = function() {
-      return "<ul>" + this.list + "</ul>";
-     };
-
+  var noteListView = function(list) {
+    this.list = list;
+  };
+  noteListView.prototype.displayPage = function() {
+    var display = "<ul>";
+    this.list.listItems.forEach(function(note) {
+      display += ("<li><div>" + note.text + "</div></li>");
+    });
+    return (display + "</ul>");
+  };
   exports.noteListView = noteListView;
 })(this);
--- note-list-view_Spec.js
+++ note-list-view_Spec.js
@@ -1,6 +1,7 @@
 (function() {
   var list = new listOfNotes();
-  var note = new Note("<ul><li><div>Favourite food: pesto</div></li><li><div>Favourite drink: seltzer</div></li></ul>");
-  list.writeNote();
-  assert.isEqual(noteListView(list).displayPage(), "<ul><li><div>Favourite food: pesto</div></li><li><div>Favourite drink: seltzer</div></li></ul>");
+  var view = new noteListView(list);
+  list.writeNote("Favourite food: pesto");
+  list.writeNote("Favourite drink: seltzer");
+  assert.isEqual(view.displayPage(), "<ul><li><div>Favourite food: pesto</div></li><li><div>Favourite drink: seltzer</div></li></ul>");
 })();
